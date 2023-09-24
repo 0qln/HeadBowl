3c67c950b8b94054655b4b511ff4be8347b5fae5
Linus Nagel <129159411+0qln@users.noreply.github.com>
2023-09-24 14:11:27 +0200

    removed complexity

--- HeadBowl/Layers/ReLULayer.cs
+++ HeadBowl/Layers/ReLULayer.cs
@@ -15,28 +15,23 @@ namespace HeadBowl.Layers
     {
         public int Size => _instance.Size;
 
-        int ILayerBuilder<TPrecision>.Size => throw new NotImplementedException();
-
         private readonly ILayer<TPrecision> _instance;
         private ILayer<TPrecision>? _next, _prev;
 
 
-
         public ReLULayerBuilder(int size)
         {
-            _instance = new ReLULayer<TPrecision>(size);
+            _instance =
+                typeof(TPrecision) == typeof(Half) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_16bit((ILayer<Half>)prevLayer, (ILayer<Half>)nextLayer, size)*/ :
+                typeof(TPrecision) == typeof(float) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_32bit((ILayer<Single>)prevLayer, (ILayer<Single>)nextLayer, size)*/ :
+                typeof(TPrecision) == typeof(double) ? (ILayer<TPrecision>)new ReLULayer_64bit(size) :
+                typeof(TPrecision) == typeof(decimal) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_128bit((ILayer<Decimal>)prevLayer, (ILayer<Decimal>)nextLayer, size)*/
+                : throw new NotImplementedException();
         }
 
 
-        void ILayerBuilder<TPrecision>.SetNext(ILayer<TPrecision>? layer)
-        {
-            _next = layer;
-        }
-
-        void ILayerBuilder<TPrecision>.SetPrev(ILayer<TPrecision>? layer)
-        {
-            _prev = layer;
-        }
+        void ILayerBuilder<TPrecision>.SetNext(ILayer<TPrecision>? layer) => _next = layer;
+        void ILayerBuilder<TPrecision>.SetPrev(ILayer<TPrecision>? layer) => _prev = layer;
 
         ILayer<TPrecision> ILayerBuilder<TPrecision>.Build()
         {
@@ -49,59 +44,6 @@ namespace HeadBowl.Layers
             return _instance;
         }
     }
-
-
-    public class ReLULayer<TPrecision> : ILayer<TPrecision>
-    {
-        private ILayer<TPrecision> _layer;
-
-
-
-        public int Size => _layer.Size;
-        public TPrecision[,] Weights => _layer.Weights;
-        public TPrecision[] Activations => _layer.Activations;
-        public TPrecision[] Gradients => _layer.Gradients;
-        public bool IsOutputLayer => _layer.IsOutputLayer;
-        public bool IsInputLayer => _layer.IsInputLayer;
-
-
-        public void ApplyGradients() 
-            => _layer.ApplyGradients();
-
-        public void Forward(in TPrecision[]? inputs) 
-            => _layer.Forward(inputs);
-
-        public void GenerateGradients(in TPrecision[]? expectedNNOutputs = null) 
-            => _layer.GenerateGradients(expectedNNOutputs);
-
-        void ILayer<TPrecision>._InitInNet(ILayer<TPrecision>? prev, ILayer<TPrecision>? next) 
-            => _layer._InitInNet(prev, next);
-
-
-        public ReLULayer(int size)
-        {
-            _layer =
-                typeof(TPrecision) == typeof(Half) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_16bit((ILayer<Half>)prevLayer, (ILayer<Half>)nextLayer, size)*/ :
-                typeof(TPrecision) == typeof(float) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_32bit((ILayer<Single>)prevLayer, (ILayer<Single>)nextLayer, size)*/ :
-                typeof(TPrecision) == typeof(double) ? (ILayer<TPrecision>)new ReLULayer_64bit(size) :
-                typeof(TPrecision) == typeof(decimal) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_128bit((ILayer<Decimal>)prevLayer, (ILayer<Decimal>)nextLayer, size)*/
-                : throw new NotImplementedException();
-        }
-
-        public ReLULayer(
-            ILayer<TPrecision> prevLayer,
-            ILayer<TPrecision> nextLayer,
-            int size)
-        {
-            _layer =
-                typeof(TPrecision) == typeof(Half) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_16bit((ILayer<Half>)prevLayer, (ILayer<Half>)nextLayer, size)*/ :
-                typeof(TPrecision) == typeof(float) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_32bit((ILayer<Single>)prevLayer, (ILayer<Single>)nextLayer, size)*/ :
-                typeof(TPrecision) == typeof(double) ? (ILayer<TPrecision>)new ReLULayer_64bit(size, (ILayer<double>)prevLayer, (ILayer<double>)nextLayer) :
-                typeof(TPrecision) == typeof(decimal) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_128bit((ILayer<Decimal>)prevLayer, (ILayer<Decimal>)nextLayer, size)*/
-                : throw new NotImplementedException();
-        }
-    }
-
 
     internal class ReLULayer_64bit : LayerBase_64bit, ILayer<double>
     {
--- HeadBowl/Layers/SigmoidLayer.cs
+++ HeadBowl/Layers/SigmoidLayer.cs
@@ -13,28 +13,23 @@ namespace HeadBowl.Layers
     {
         public int Size => _instance.Size;
 
-        int ILayerBuilder<TPrecision>.Size => throw new NotImplementedException();
-
         private readonly ILayer<TPrecision> _instance;
         private ILayer<TPrecision>? _next, _prev;
 
 
-
         public SigmoidLayerBuilder(int size)
         {
-            _instance = new SigmoidLayer<TPrecision>(size);
+            _instance =
+                typeof(TPrecision) == typeof(Half) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_16bit((ILayer<Half>)prevLayer, (ILayer<Half>)nextLayer, size)*/ :
+                typeof(TPrecision) == typeof(float) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_32bit((ILayer<Single>)prevLayer, (ILayer<Single>)nextLayer, size)*/ :
+                typeof(TPrecision) == typeof(double) ? (ILayer<TPrecision>)new SigmoidLayer_64bit(size) :
+                typeof(TPrecision) == typeof(decimal) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_128bit((ILayer<Decimal>)prevLayer, (ILayer<Decimal>)nextLayer, size)*/
+                : throw new NotImplementedException();
         }
 
 
-        void ILayerBuilder<TPrecision>.SetNext(ILayer<TPrecision>? layer)
-        {
-            _next = layer;
-        }
-
-        void ILayerBuilder<TPrecision>.SetPrev(ILayer<TPrecision>? layer)
-        {
-            _prev = layer;
-        }
+        void ILayerBuilder<TPrecision>.SetNext(ILayer<TPrecision>? layer) => _next = layer;
+        void ILayerBuilder<TPrecision>.SetPrev(ILayer<TPrecision>? layer) => _prev = layer;
 
         ILayer<TPrecision> ILayerBuilder<TPrecision>.Build()
         {
@@ -47,59 +42,6 @@ namespace HeadBowl.Layers
             return _instance;
         }
     }
-
-
-    public class SigmoidLayer<TPrecision> : ILayer<TPrecision>
-    {
-        private ILayer<TPrecision> _layer;
-
-
-
-        public int Size => _layer.Size;
-        public TPrecision[,] Weights => _layer.Weights;
-        public TPrecision[] Activations => _layer.Activations;
-        public TPrecision[] Gradients => _layer.Gradients;
-        public bool IsOutputLayer => _layer.IsOutputLayer;
-        public bool IsInputLayer => _layer.IsInputLayer;
-
-
-        public void ApplyGradients()
-            => _layer.ApplyGradients();
-
-        public void Forward(in TPrecision[]? inputs)
-            => _layer.Forward(inputs);
-
-        public void GenerateGradients(in TPrecision[]? expectedNNOutputs = null)
-            => _layer.GenerateGradients(expectedNNOutputs);
-
-        void ILayer<TPrecision>._InitInNet(ILayer<TPrecision>? prev, ILayer<TPrecision>? next)
-            => _layer._InitInNet(prev, next);
-
-
-        public SigmoidLayer(int size)
-        {
-            _layer =
-                typeof(TPrecision) == typeof(Half) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_16bit((ILayer<Half>)prevLayer, (ILayer<Half>)nextLayer, size)*/ :
-                typeof(TPrecision) == typeof(float) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_32bit((ILayer<Single>)prevLayer, (ILayer<Single>)nextLayer, size)*/ :
-                typeof(TPrecision) == typeof(double) ? (ILayer<TPrecision>)new SigmoidLayer_64bit(size) :
-                typeof(TPrecision) == typeof(decimal) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_128bit((ILayer<Decimal>)prevLayer, (ILayer<Decimal>)nextLayer, size)*/
-                : throw new NotImplementedException();
-        }
-
-        public SigmoidLayer(
-            ILayer<TPrecision> prevLayer,
-            ILayer<TPrecision> nextLayer,
-            int size)
-        {
-            _layer =
-                typeof(TPrecision) == typeof(Half) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_16bit((ILayer<Half>)prevLayer, (ILayer<Half>)nextLayer, size)*/ :
-                typeof(TPrecision) == typeof(float) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_32bit((ILayer<Single>)prevLayer, (ILayer<Single>)nextLayer, size)*/ :
-                typeof(TPrecision) == typeof(double) ? (ILayer<TPrecision>)new SigmoidLayer_64bit(size, (ILayer<double>)prevLayer, (ILayer<double>)nextLayer) :
-                typeof(TPrecision) == typeof(decimal) ? throw new NotImplementedException() /*(ILayer<T>)new ReLULayer_128bit((ILayer<Decimal>)prevLayer, (ILayer<Decimal>)nextLayer, size)*/
-                : throw new NotImplementedException();
-        }
-    }
-
 
     internal class SigmoidLayer_64bit : LayerBase_64bit, ILayer<double>
     {
@@ -119,5 +61,4 @@ namespace HeadBowl.Layers
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override double ActivationDerivative(double input) => input * (1 - input); // when this is used, the input is already sigmoided.
     }
-
 }
