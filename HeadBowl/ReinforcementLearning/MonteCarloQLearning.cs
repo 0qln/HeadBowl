a53d7575a60d554b5897f7cfdd3b0d7ff2d0434b
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-03-30 03:34:38 +0100

    Monte Carlo Q Learning

---
+++ HeadBowl/ReinforcementLearning/MonteCarloQLearning.cs
@@ -0,0 +1,31 @@
+﻿namespace HeadBowl.ReinforcementLearning;
+
+/// <summary>
+/// https://github.com/0qln/Obsidian/blob/main/Main/Coding/Q%20Learning.md#monte-carlo-learning
+/// </summary>
+/// <typeparam name="TPrecision"></typeparam>
+/// <typeparam name="TState"></typeparam>
+/// <typeparam name="TAction"></typeparam>
+public static class MonteCarloQLearning<TPrecision, TState, TAction>
+    where TPrecision : struct
+{
+    /// <summary>
+    /// Quality of the State/Action pair.
+    /// </summary>
+    /// <typeparam name="TState"></typeparam>
+    /// <typeparam name="TAction"></typeparam>
+    /// <param name="state">Current state.</param>
+    /// <param name="action">The action to take.</param>
+    /// <returns>The quality of the state s, taken action a.</returns>
+    public delegate TPrecision QFunction(TState state, TAction action);
+
+    // TODO:
+    // The new function will get slower and slower over each episode of training.
+    // Unwrap the recursive implementation and update the function iteratively instead.
+    public static QFunction NewQFunction(QFunction oldQFunction, TPrecision totalRewardOverEpisode, int numStates) 
+    => (state, action) =>
+    {
+        TPrecision qOld = oldQFunction(state, action);
+        return qOld + (1.0f / numStates) * ((dynamic)totalRewardOverEpisode - qOld);
+    };
+}
