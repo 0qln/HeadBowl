a84e7639549a6fef2883cc8bcf76ac1b6d524694
Linus Nagel <129159411+0qln@users.noreply.github.com>
2024-04-01 02:06:28 +0200

    Update Mnist.cs

--- HeadBowl/TrainingData/Mnist.cs
+++ HeadBowl/TrainingData/Mnist.cs
@@ -1,19 +1,36 @@
+using HeadBowl.Helpers;
+using System.Linq;
+
 namespace HeadBowl.TrainingData;
 
-public class Mnist<TPrecision> : ITrainingData<TPrecision>, ITestingData<TPrecision>
+public class Mnist<TPrecision>(string trainImagesPath, string testImagesPath, string trainLabelsPath, string testLabelsPath) : ITrainingData<TPrecision>, ITestingData<TPrecision>
 {
-    private readonly DataInstance<TPrecision>[] _trainingData, _testingData;
+    public int InputSize => 28 * 28;
+    public int OutputSize => 1;
 
-    public int InputSize => throw new NotImplementedException();
-    public int OutputSize => throw new NotImplementedException();
-    public int SampleCount => throw new NotImplementedException();
+    private readonly DataInstance<TPrecision>[]
+        _trainingData = Mnist<TPrecision>.LoadData(trainImagesPath, trainLabelsPath),
+        _testingData = Mnist<TPrecision>.LoadData(testImagesPath, testLabelsPath);
 
     DataInstance<TPrecision>[] ITestingData<TPrecision>.Data => _testingData;
     DataInstance<TPrecision>[] ITrainingData<TPrecision>.Data => _trainingData;
 
+    int ITrainingData<TPrecision>.SampleCount => _trainingData.Length;
+    int ITestingData<TPrecision>.SampleCount => _testingData.Length;
 
-    public Mnist(string trainImages, string testImages, string trainLabels, string testLabels)
-    {
-        throw new NotImplementedException();
-    }
+    private static DataInstance<TPrecision>[] LoadData(string imagesPath, string labelsPath) => 
+        // Combine Imagees with their coresponding labels.
+        Enumerable.Zip(
+            // Images
+            IDX.Decode<TPrecision>(imagesPath)
+               .Cast<Array>()
+               .Select(arr => arr.Cast<TPrecision[]>()),
+            // Lables
+            IDX.Decode<TPrecision>(labelsPath)
+               .Cast<TPrecision>())
+        // Create a data instace.
+        .Select(x => new DataInstance<TPrecision>(
+            x.First.SelectMany(y => y).ToArray(),
+            [x.Second]))
+        .ToArray();
 }
